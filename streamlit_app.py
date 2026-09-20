aa4eb97b30f02f76342e0300f3a315f8d83f3b61
ibrahimmustafe995-maker <ibrahimmustafe995@gmail.com>
2026-09-20 19:45:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
- import streamlit as st
-# Green Hope University — Garowe Campus
+ # Green Hope University — Garowe Campus
 ## Freshman Orientation & Welcome Portal
 
 A polished **Streamlit** web application that welcomes new students to the Garowe
